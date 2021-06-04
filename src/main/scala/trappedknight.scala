304c56658d9aaf82d538a2bbe2d07ca41c8eced1
charlibot <charlibot@protonmail.com>
2021-06-04 23:53:34 +0200

    words

--- src/main/scala/trappedknight.scala
+++ src/main/scala/trappedknight.scala
@@ -2,7 +2,9 @@ case class Cell(i: Int, j: Int)
 
 // can be simplified: https://github.com/buddhabrot/UlamAnalysis/blob/main/README.md
 // https://medium.com/swlh/the-trapped-knight-revisited-132c034a8a28
-// although everything can be written in terms of `d` but I find it easier to reason about to keep it where you move around the square
+// Although the formulae in the match can be written in terms of `d`,
+// I find it easier to keep it as-is in order to reason about
+// how to find the value moving around the edge of the square.
 def valueAt(cell: Cell): Int = 
   val d = Math.max(Math.abs(cell.i), Math.abs(cell.j))
   val sideLength = 2 * d + 1
@@ -19,20 +21,20 @@ def valueAt(cell: Cell): Int =
 import cats.implicits._
 import cats.Applicative
 def possiblePositions(position: Cell): Set[Cell] = 
-  val x = List(2, 1, -2, -1)
+  val movements = List(2, 1, -2, -1)
   Applicative[List]
-    .product(x, x)
+    .product(movements, movements)
     .filterNot(Math.abs(_) == Math.abs(_))
     .map((x, y) => Cell(position.i + x, position.j + y))
     .toSet
 
-def determineEnd(current: Cell, seen: Set[Cell]): Int = 
+def trapKnight(current: Cell, seen: Set[Cell]): Int = 
   val p = possiblePositions(current)
   // find the Cells that have not been seen
   val x = p diff seen
   if (x.isEmpty) valueAt(current)
-  else determineEnd(x.map(c => (valueAt(c), c)).toList.sortBy(_._1).head._2, seen + current)
+  else trapKnight(x.map(c => (valueAt(c), c)).toList.sortBy(_._1).head._2, seen + current)
 
 @main
 def main =
-  println(determineEnd(Cell(0, 0), Set()))
+  println(trapKnight(Cell(0, 0), Set()))
